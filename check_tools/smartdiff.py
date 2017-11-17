e11a262c3235b0bffef9501c7facd7afb83a77bd
Kevin Gilbert <kevin.gilbert@arm.com>
2017-11-17 12:07:22 -0600

    Reorganize exception blocks

--- check_tools/smartdiff.py
+++ check_tools/smartdiff.py
@@ -25,31 +25,26 @@ def main():
         # Split the line number and trailing newlines from the list of found prohibited words
         try:
             # The compact list mode of the validation script only lists files that contained prohibited
-            # words. If a key error is found here, the head revision contained no prohibited words. Fill
-            # with empty list to compare the PR revision against
+            # words. If a key error is found here, either the head revision contained no prohibited words, a
+            # new file was added, or an existing file was moved or renamed. Fill with empty list to
+            # compare the PR revision against
             stripped_head_words = [word.split(',')[1].replace("\n","") for word in head_dict[doc]]
         except KeyError:
             stripped_head_words = []
 
-        try:
-            # Check for move in file location as the path is included in the key. Catch on try failure
-            stripped_pr_words = [word.split(',')[1].replace("\n","") for word in words]
+        stripped_pr_words = [word.split(',')[1].replace("\n","") for word in words]
 
-            # Do not want to trigger failure on removed prohibited words from existing docs.
-            # Subtracting the set of PR words from the head revision list works as this filter
-            diff = list(set(stripped_pr_words) - set(stripped_head_words))
+        # Do not want to trigger failure on removed prohibited words from existing docs.
+        # Subtracting the set of PR words from the head revision list works as this filter
+        diff = list(set(stripped_pr_words) - set(stripped_head_words))
 
-            if diff:
-                print "Found added words from the prohibited list in file %s..." % doc
-                result = -1
-                for word in diff:
-                    # Find word in unstripped set to pull in line number
-                    line = words[diff.index(word)].split(',')[0]
-                    print "\t- Line: %s, Word: %s" % (line, word)
-
-        except KeyError as E:
-            print "File name or location change, skipping file: %s" % E
+        if diff:
+            print "Found added words from the prohibited list in file %s..." % doc
             result = -1
+            for word in diff:
+                # Find word in unstripped set to pull in line number
+                line = words[diff.index(word)].split(',')[0]
+                print "\t- Line: %s, Word: %s" % (line, word)
 
     sys.exit(result)
 
