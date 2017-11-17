21c61695cca436523daa0e4c028dd3ac4bd139f4
Kevin Gilbert <kevin.gilbert@arm.com>
2017-11-17 11:59:34 -0600

    Resolve minor errors -
    Head revision files with no errors were not included in the compact list form, threw key errors and skipped comparing against the PR list of prohibited words

--- check_tools/smartdiff.py
+++ check_tools/smartdiff.py
@@ -23,25 +23,33 @@ def main():
 
     for doc,words in pr_dict.iteritems():
         # Split the line number and trailing newlines from the list of found prohibited words
-        # Check for move in file location as the path is included in the key. Catch on try failure
         try:
-            stripped_pr_words = [word.split(',')[1].replace("\n","") for word in words]
+            # The compact list mode of the validation script only lists files that contained prohibited
+            # words. If a key error is found here, the head revision contained no prohibited words. Fill
+            # with empty list to compare the PR revision against
             stripped_head_words = [word.split(',')[1].replace("\n","") for word in head_dict[doc]]
+        except KeyError:
+            stripped_head_words = []
+
+        try:
+            # Check for move in file location as the path is included in the key. Catch on try failure
+            stripped_pr_words = [word.split(',')[1].replace("\n","") for word in words]
+
+            # Do not want to trigger failure on removed prohibited words from existing docs.
+            # Subtracting the set of PR words from the head revision list works as this filter
+            diff = list(set(stripped_pr_words) - set(stripped_head_words))
+
+            if diff:
+                print "Found added words from the prohibited list in file %s..." % doc
+                result = -1
+                for word in diff:
+                    # Find word in unstripped set to pull in line number
+                    line = words[diff.index(word)].split(',')[0]
+                    print "\t- Line: %s, Word: %s" % (line, word)
+
         except KeyError as E:
             print "File name or location change, skipping file: %s" % E
-            result += 1
-
-        # Do not want to trigger failure on removed prohibited words from existing docs.
-        # Subtracting the set of PR words from the head revision list works as this filter
-        diff = list(set(stripped_pr_words) - set(stripped_head_words))
-
-        if diff:
-            print "Found added words from the prohibited list in file %s..." % doc
-            result += 2
-            for word in diff:
-                # Find word in unstripped set to pull in line number
-                line = words[diff.index(word)].split(',')[0]
-                print "\t- Line: %s, Word: %s" % (line, word)
+            result = -1
 
     sys.exit(result)
 
